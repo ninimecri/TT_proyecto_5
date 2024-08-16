ac223cb87d739c5b12a84b3da219742a6be9498e
ninimecri <nicole-mejia@hotmail.com>
2024-08-16 16:20:01 +1000

    Data frame de vehiculos usados

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-car_data = pd.read_csv('streamlit/vehicles_us.csv')  # leer los datos
+car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 
 print(car_data.head())
 
