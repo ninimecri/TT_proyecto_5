1e28d1ddc20dd79adadf4788feb4ba9f64fe4421
ninimecri <nicole-mejia@hotmail.com>
2024-08-17 23:42:21 +1000

    Codigo para la aplicacion web

--- app.py
+++ app.py
@@ -11,17 +11,29 @@ hist_button = st.button('Construccion histograma')
 
 # Botón para generar el histograma
 if st.button('Mostrar Histograma'):
-    st.write("Histograma de la columna 'x'")
-    fig = px.histogram(car_data, x='x', nbins=5)
+    st.write("Histograma de la distribución de Precios")
+    fig = px.histogram(car_data, x="price", title="Distribución de Precios de Vehículos",
+                       color_discrete_sequence=px.colors.qualitative.Set1)  # crear un histograma
     st.plotly_chart(fig)
 
-scatter_button = st.button('Construccion gráfico de dispersión')
+scatter_button = st.button('Construccion gráfico de dispersión aqui')
 
 # Botón para generar el gráfico de dispersión
+car_data['condition'] = car_data['condition'].astype('category')
+
 if st.button('Mostrar Gráfico de Dispersión'):
 
-    st.write("Gráfico de dispersión de 'x' contra 'y'")
-    fig = px.scatter(car_data, x='x', y='y')
+    st.write("Gráfico de dispersión de 'Precio' contra 'Condicion'")
+
+    # Crear el gráfico de dispersión con diferenciación de colores basada en 'condition'
+    fig = px.scatter(car_data,
+                     x='price',
+                     y='condition',
+                     color='condition',
+                     title="Precio vs Condición de Vehículos",
+                     hover_data=["model_year", "odometer"],
+                     color_discrete_sequence=px.colors.qualitative.Set2)  # Colores claros
+
     st.plotly_chart(fig)
 
 st.write("### Desafío Extra: Generar Gráficos con Casillas de Verificación")
@@ -29,5 +41,6 @@ st.write("### Desafío Extra: Generar Gráficos con Casillas de Verificación")
 # Casilla de verificación para el histograma
 if st.checkbox('Generar Histograma'):
     st.write("Histograma de la columna 'x'")
-    fig = px.histogram(car_data, x='x', nbins=5, title='Histograma de X')
+    fig = px.histogram(car_data, x="price", title="Distribución de Precios de Vehículos",
+                       color_discrete_sequence=px.colors.qualitative.Set1)
     st.plotly_chart(fig)
